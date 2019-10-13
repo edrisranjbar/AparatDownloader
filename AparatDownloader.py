d5cd5d2404b20304ae6511b41cdeae0c32dd48c7
edris <edris.qeshm2@gmail.com>
2019-10-13 22:30:00 +0330

    Download with the name of page as file name, and some othe changes

--- AparatDownloader.py
+++ AparatDownloader.py
@@ -1,16 +1,23 @@
 # Import libraries
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import wget
+import wget,os
 
-url = "https://www.aparat.com/v/Z9OH0"
-savePath = "/home/edris/Dev/Aparat"
-browser = webdriver.Chrome(executable_path="/home/edris/Dev/Aparat/chromedriver")
+currentFolder = os.getcwd()
+urls = []
 
-browser.get(url)
+# Getting URLs
+userUrl = input("Give me the URL: (N for exit getting URL)")
+while userUrl!="n":
+    urls.append(userUrl)
+    userUrl = input("Give me the URL: (N for exit getting URL)")
 
-browser.find_element_by_xpath("/html/body/main/div[1]/div/div/div/div/section[2]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/button").click()
-DownloadLink = browser.find_elements_by_css_selector(".dropdown .dropdown-content .menu-wrapper .menu-list .menu-item-link a")[4]
-DownloadLink = DownloadLink.get_attribute("href")
-DownloadStatus = wget.download(DownloadLink)
-#browser.close()
+browser = webdriver.Chrome(executable_path="{0}/chromedriver".format(currentFolder))
+for url in urls:
+    browser.get(url)
+    title = browser.title
+    browser.find_element_by_xpath("/html/body/main/div[1]/div/div/div/div/section[2]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/button").click()
+    DownloadLink = browser.find_elements_by_css_selector(".dropdown .dropdown-content .menu-wrapper .menu-list .menu-item-link a")[4]
+    DownloadLink = DownloadLink.get_attribute("href")
+    DownloadStatus = wget.download(DownloadLink,currentFolder+"/"+title+".mp4")
+    browser.close()
