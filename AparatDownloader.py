c1fe028e3b78a32758b9575d9c309475c45f6719
edris <edris.qeshm2@gmail.com>
2019-10-13 21:00:13 +0330

    Some changes

--- AparatDownloader.py
+++ AparatDownloader.py
@@ -1,6 +1,6 @@
 # Import libraries
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
-browser.get("https://edrisranjbar.ir")
-
+browser = webdriver.Chrome(executable_path="/home/edris/Dev/Aparat/chromedriver")
+browser.get("http://edrisranjbar.ir")
