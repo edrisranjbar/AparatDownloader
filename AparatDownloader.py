6af039e4528c4e435a1f638b780c51ab20906404
edris <edris.qeshm2@gmail.com>
2019-10-13 21:57:11 +0330

    Simplified

--- AparatDownloader.py
+++ AparatDownloader.py
@@ -12,11 +12,5 @@ browser.get(url)
 browser.find_element_by_xpath("/html/body/main/div[1]/div/div/div/div/section[2]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/button").click()
 DownloadLink = browser.find_elements_by_css_selector(".dropdown .dropdown-content .menu-wrapper .menu-list .menu-item-link a")[4]
 DownloadLink = DownloadLink.get_attribute("href")
-print("Download {0} Started! \n".format(DownloadLink))
 DownloadStatus = wget.download(DownloadLink)
-while DownloadLink == False:
-    print("Downloading... \n")
-    
-else:
-    print("Download is Done \n!")
 #browser.close()
