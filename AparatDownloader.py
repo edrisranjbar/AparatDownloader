c3dc4c07e7c2d44ae619ae6b7321df4a73e639a9
edris <edris.qeshm2@gmail.com>
2019-10-14 20:26:27 +0330

    count of downloaded videos and error  providing

--- AparatDownloader.py
+++ AparatDownloader.py
@@ -5,19 +5,50 @@ import wget,os
 
 currentFolder = os.getcwd()
 urls = []
+count = 0
 
 # Getting URLs
-userUrl = input("Give me the URL: (N for exit getting URL)")
-while userUrl!="n":
+def getUrl():
+    message ="Give me the URL: (N for exit getting URL)"
+    # If it is a NO or something like this
+    if len(message) < 5:
+        return input(message).lower()
+    return input(message)
+
+# Download Based on URL
+def downloadWithUrl(url):
+    try:
+        browser.get(url)
+        title = browser.title
+        try:
+            # Download button
+            browser.find_element_by_xpath("/html/body/main/div[1]/div/div/div/div/section[2]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/button").click()
+            
+            # Get Download link with 720p
+            DownloadLink = browser.find_elements_by_css_selector(".dropdown .dropdown-content .menu-wrapper .menu-list .menu-item-link a")[4]
+            DownloadLink = DownloadLink.get_attribute("href")
+            
+            # Download the video with the name of page
+            wget.download(DownloadLink,currentFolder+"/"+title+".mp4")
+        except:
+            print("\n Something went wrong")
+            return
+    except:
+        print("\n URL is invalid or no internet")
+        return
+    return True
+
+
+userUrl = getUrl()
+while userUrl!="n" and userUrl != "no":
     urls.append(userUrl)
-    userUrl = input("Give me the URL: (N for exit getting URL)")
+    userUrl = getUrl()
 
 browser = webdriver.Chrome(executable_path="{0}/chromedriver".format(currentFolder))
 for url in urls:
-    browser.get(url)
-    title = browser.title
-    browser.find_element_by_xpath("/html/body/main/div[1]/div/div/div/div/section[2]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/button").click()
-    DownloadLink = browser.find_elements_by_css_selector(".dropdown .dropdown-content .menu-wrapper .menu-list .menu-item-link a")[4]
-    DownloadLink = DownloadLink.get_attribute("href")
-    DownloadStatus = wget.download(DownloadLink,currentFolder+"/"+title+".mp4")
+    status = downloadWithUrl(url)
+    if status:
+        count += 1
 browser.close()
+print("---------------------------------")
+print("Count of downloaded videos: " + str(count))
