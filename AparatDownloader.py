562ccd3014278842d73cc72c06e26c328ba61d28
edris <edris.qeshm2@gmail.com>
2019-10-17 00:19:32 +0330

    OOP and Download all videos in a play list

--- AparatDownloader.py
+++ AparatDownloader.py
@@ -1,68 +1,74 @@
 # Import libraries
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import wget,os
-
-currentFolder = os.getcwd()
-urls = []
-count = 0
-
-# URL Validation
-def isValid(url):
-    if "https://www.aparat.com/" in url:
-        return True
-    else:
-        return False
+import wget
+import os
 
 
-# Getting URLs
-def getUrl():
-    message ="Give me the URL: (N or NO for exit getting URL)"
-    userUrl = input(message)
-    if len(userUrl) > 2:
-        if isValid(userUrl):
-            urls.append(userUrl)
-            getUrl()
+class AparatDownloader():
+    def __init__(self):
+        self.currentFolder = os.getcwd()
+        self.url = ""
+        self.count = 0
+        self.videoLinks = []
+
+    def isValid(self, url):
+        if "https://www.aparat.com/" in url:
+            return True
         else:
             print("URL is invalid!")
-    else:
-        userUrl.lower()
-        if userUrl == "n" or userUrl == "no":
-            return
+            self.getUrl()
 
-        
-# Download Based on URL
-def downloadWithUrl(url):
-    try:
-        browser.get(url)
-        title = browser.title
+    def getUrl(self):
+        userUrl = input("Give me the URL: ")
+        if self.isValid(userUrl):
+            self.url = userUrl
+
+    def downloadWithUrl(self, url):
         try:
-            # Download button
-            browser.find_element_by_xpath("/html/body/main/div[1]/div/div/div/div/section[2]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/button").click()
-            
-            # Get Download link with 720p
-            DownloadLink = browser.find_elements_by_css_selector(".dropdown .dropdown-content .menu-wrapper .menu-list .menu-item-link a")[4]
-            DownloadLink = DownloadLink.get_attribute("href")
-            
-            # Download the video with the name of page
-            wget.download(DownloadLink,currentFolder+"/"+title+".mp4")
+            browser.get(url)
+            title = browser.title
+            try:
+                # Download button
+                browser.find_element_by_xpath(
+                    "/html/body/main/div[1]/div/div/div/div/section[2]/div/div[2]/div[2]/div[2]/div[1]/div[1]/div/button").click()
+
+                # Get Download link with 720p
+                DownloadLink = browser.find_elements_by_css_selector(
+                    ".dropdown .dropdown-content .menu-wrapper .menu-list .menu-item-link a")[4]
+                DownloadLink = DownloadLink.get_attribute("href")
+
+                # Download the video with the name of page
+                wget.download(DownloadLink, self.currentFolder +
+                              "/"+title+".mp4")
+            except:
+                print("\n Download link was not found")
+                return False
         except:
-            print("\n Download link was not found")
+            print("\n URL is invalid or no internet")
             return False
-    except:
-        print("\n URL is invalid or no internet")
-        return False
-    return True
+        return True
+
+    def downloadFromPlayList(self, url):
+        browser.get(url)
+        links = browser.find_elements_by_css_selector(
+            ".playlist-body .thumb-title a")
+        for link in links:
+            self.videoLinks.append(link.get_attribute("href"))
+
+        for link in self.videoLinks:
+            self.downloadWithUrl(link)
+            self.count += 1
 
 
-browser = webdriver.Chrome(executable_path="{0}/chromedriver".format(currentFolder))
-getUrl()
+downloader = AparatDownloader()
 
-for url in urls:
-    status = downloadWithUrl(url)
-    if status:
-        count += 1
+browser = webdriver.Chrome(
+    executable_path="{0}/chromedriver".format(downloader.currentFolder))
+
+downloader.getUrl()
+downloader.downloadFromPlayList(downloader.url)
 
 browser.close()
 print("---------------------------------")
-print("Count of downloaded videos: " + str(count))
+print("Count of downloaded videos: " + str(downloader.count))
