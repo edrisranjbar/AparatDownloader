92221ccdb67e2f1c941939f149e4049cc37b0c55
edris <edris.qeshm2@gmail.com>
2022-08-09 14:00:38 +0430

    Separate core scrapping functionality from GUI #6

--- AparatDownloader.py
+++ AparatDownloader.py
@@ -1,70 +1,16 @@
-# Import libraries
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-import wget
-import os
 from tkinter import *
-from time import sleep
+from scrapper import Scrapper
+
+downloader = Scrapper()
 
 
-class AparatDownloader():
-    def __init__(self):
-        self.currentFolder = os.getcwd()
-        self.url = ""
-        self.count = 0
-        self.videoLinks = []
-        self.errors = ""
-
-    def isValid(self, url):
-        if "https://www.aparat.com/" in url:
-            self.errors = ""
-            return True
-        else:
-            self.errors = "URL is invalid!"
-            return False
-
-    def downloadWithUrl(self, url):
-        try:
-            browser.get(url)
-            sleep(6)
-            title = browser.title
-            try:
-                # Download button
-                browser.find_element(By.CSS_SELECTOR,
-                                     "#primary > div.single-details > div.single-details__info > div.single-details__utils > div > div > div.download-button > div > div > button").click()
-
-                # Get Download link with 720p
-                DownloadLink = browser.find_element(By.XPATH,
-                                                    "//*[@id='720p']/div/span/span").click()
-                sleep(3)
-                browser.switch_to.window(browser.window_handles[-1])
-                DownloadLink = browser.current_url
-
-                # Download the video with the name of page
-                wget.download(DownloadLink, self.currentFolder +
-                              "/"+title+".mp4")
-            except:
-                self.errors = "\n Download link was not found"
-                return False
-        except:
-            self.errors = "\n URL is invalid or no internet"
-            return False
-        return True
-
-    def downloadFromPlayList(self, url):
-        browser.get(url)
-        sleep(10)
-        links = browser.find_elements(By.CSS_SELECTOR,
-                                      "a.titled-link.title[data-refer=playlists]")
-        for link in links:
-            self.videoLinks.append(link.get_attribute("href"))
-
-        for link in self.videoLinks:
-            self.downloadWithUrl(link)
-            self.count += 1
+def download():
+    if downloader.isValid(txtUrl.get()):
+        downloader.downloadFromPlayList(txtUrl.get())
+        lblCount['text'] = "Count of downloaded videos:" + \
+            str(downloader.count)
+    print(downloader.errors)
+    lblErrors['text'] = downloader.errors
 
 
 # Inititalizing UI
@@ -79,26 +25,5 @@ txtUrl = Entry(root)
 txtUrl.pack()
 lblCount = Label(root, text="Count of downloaded videos:")
 lblCount.pack()
-
-
-def download():
-    if downloader.isValid(txtUrl.get()):
-        downloader.downloadFromPlayList(txtUrl.get())
-        lblCount['text'] = "Count of downloaded videos:" + \
-            str(downloader.count)
-    print(downloader.errors)
-    lblErrors['text'] = downloader.errors
-
-
-downloader = AparatDownloader()
-
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-options.add_argument('headless')
-browser = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install())
-                           )
-
 Button(root, text="Download", command=download).pack()
-
 root.mainloop()
-browser.close()
---
+++ scrapper.py
@@ -0,0 +1,74 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+import wget
+import os
+
+from time import sleep
+
+
+class Scrapper():
+    def __init__(self):
+        self.currentFolder = os.getcwd()
+        self.url = ""
+        self.count = 0
+        self.videoLinks = []
+        self.errors = ""
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option(
+            "excludeSwitches", ["enable-logging"])
+        self.options.add_argument('headless')
+        self.browser = webdriver.Chrome(options=self.options, service=Service(ChromeDriverManager().install())
+                                        )
+
+    def isValid(self, url):
+        if "https://www.aparat.com/" in url:
+            self.errors = ""
+            return True
+        else:
+            self.errors = "URL is invalid!"
+            return False
+
+    def downloadWithUrl(self, url):
+        try:
+            self.browser.get(url)
+            sleep(6)
+            title = self.browser.title
+            try:
+                # Download button
+                self.browser.find_element(By.CSS_SELECTOR,
+                                          "#primary > div.single-details > div.single-details__info > div.single-details__utils > div > div > div.download-button > div > div > button").click()
+
+                # Get Download link with 720p
+                DownloadLink = self.browser.find_element(By.XPATH,
+                                                         "//*[@id='720p']/div/span/span").click()
+                sleep(3)
+                self.browser.switch_to.window(self.browser.window_handles[-1])
+                DownloadLink = self.browser.current_url
+
+                # Download the video with the name of page
+                wget.download(DownloadLink, self.currentFolder +
+                              "/"+title+".mp4")
+            except:
+                self.errors = "\n Download link was not found"
+                return False
+        except:
+            self.errors = "\n URL is invalid or no internet"
+            return False
+        finally:
+            self.browser.close()
+        return True
+
+    def downloadFromPlayList(self, url):
+        self.browser.get(url)
+        sleep(10)
+        links = self.browser.find_elements(By.CSS_SELECTOR,
+                                           "a.titled-link.title[data-refer=playlists]")
+        for link in links:
+            self.videoLinks.append(link.get_attribute("href"))
+
+        for link in self.videoLinks:
+            self.downloadWithUrl(link)
+            self.count += 1
